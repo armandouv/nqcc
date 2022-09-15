2dbb66d5d886d0fb2104c20c3c2d12eaa0563fd7
Armando Ugalde <armauv@gmail.com>
2022-09-15 16:20:18 -0500

    Implement printing options using flags

--- lib/nqcc.ex
+++ lib/nqcc.ex
@@ -4,9 +4,10 @@ defmodule Nqcc do
   """
   @commands %{
     "help" => "Prints this help",
-    "l" => "Displays scanner output",
-    "p" => "Displays parser output",
-    "s" => "Displays code generator output"
+    "sn" => "Prints sanitizer output",
+    "l" => "Prints scanner output",
+    "p" => "Prints parser output",
+    "s" => "Prints code generator output"
   }
 
   def main(args) do
@@ -16,29 +17,46 @@ defmodule Nqcc do
   end
 
   def parse_args(args) do
-    OptionParser.parse(args, switches: [help: :boolean])
+    OptionParser.parse(args, strict: [help: :boolean, l: :boolean, p: :boolean, s: :boolean, sn: :boolean])
   end
 
   defp process_args({[help: true], _, _}) do
     print_help_message()
   end
 
-  defp process_args({_, [file_name], _}) do
-    compile_file(file_name)
+  defp process_args({parsed, [file_name], _}) do
+    flags = for {flag_name, _} <- parsed, into: MapSet.new, do: flag_name
+    compile_file(file_name, flags)
   end
 
-  defp compile_file(file_path) do
+  def inspect_output(output, flags, target_flag, label) when is_binary(output) do
+    if target_flag in flags do
+      IO.puts(label <> ":")
+      IO.puts(output)
+    end
+    output
+  end
+
+  def inspect_output(output, flags, target_flag, label) do
+    if target_flag in flags do
+      IO.inspect(output, label: label)
+    end
+    output
+  end
+
+  defp compile_file(file_path, flags) do
     IO.puts("Compiling file: " <> file_path)
     assembly_path = String.replace_trailing(file_path, ".c", ".s")
 
     File.read!(file_path)
     |> Sanitizer.sanitize_source()
-    |> IO.inspect(label: "\nSanitizer output")
+    |> inspect_output(flags, :sn, "\nSanitizer output")
     |> Lexer.scan_words()
-    |> IO.inspect(label: "\nLexer output")
+    |> inspect_output(flags, :l, "\nLexer output")
     |> Parser.parse_program()
-    |> IO.inspect(label: "\nParser output")
+    |> inspect_output(flags, :p, "\nParser output")
     |> CodeGenerator.generate_code()
+    |> inspect_output(flags, :s, "\nCode generator output")
     |> Linker.generate_binary(assembly_path)
   end
 
