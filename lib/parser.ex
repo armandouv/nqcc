bd9894bfa749761d6f63bafadc7ef920e547db44
Aldo <aldo.gomez02b@gmail.com>
2022-11-23 14:41:56 -0600

    Se agregan las funciones en el parser para que el compilador reconozca operadores unarios y binarios, teniendo en cuenta la precedencia de operadores asi como la precedencia de los parentesis

--- lib/parser.ex
+++ lib/parser.ex
@@ -93,17 +93,84 @@ defmodule Parser do
     end
   end
 
-  def parse_expression([next_token | rest]) do
-    case next_token do
-      {:constant, value} -> {%AST{node_name: :constant, value: value}, rest}
-      next_token when next_token in [:negation, :logical_negation, :bitwise_complement] ->
+  def parse_expression(tokens) do
+    parse_production(tokens, [:or], &parse_logical_and_exp/1)
+  end
+
+  def parse_logical_and_exp(tokens) do
+    parse_production(tokens, [:and], &parse_equality_exp/1)
+  end
+
+  def parse_equality_exp(tokens) do
+    parse_production(tokens, [:equal, :not_equal], &parse_relational_exp/1)
+  end
+
+  def parse_relational_exp(tokens) do
+    parse_production(tokens, [:less_than, :less_than_or_equal, :greater_than, :greater_than_or_equal], &parse_additive_exp/1)
+  end
+
+  def parse_additive_exp(tokens) do
+    parse_production(tokens, [:addition, :negation], &parse_term/1)
+  end
+
+  def parse_term(tokens) do
+    parse_production(tokens, [:multiplication, :division], &parse_factor/1)
+  end
+
+  def parse_factor([next_token | rest]) do
+    factor = case next_token do
+      :open_paren ->
         result = parse_expression(rest)
+        case result do
+          {{:parsing_error, _, _}, _rest} -> result
+          {inner_exp_node, [:close_paren | after_paren]} -> {inner_exp_node, after_paren}
+          {_inner_exp_node, rest_exp} -> {{:parsing_error, :closing_paren_missed, "Error: closing parenthesis missed"}, rest_exp}
+        end
+
+      {:constant, value} -> {%AST{node_name: :constant, value: value}, rest}
+
+      next_token when next_token in [:negation, :logical_negation, :bitwise_complement] ->
+        result = parse_factor(rest)
         case result do
           {{:parsing_error, _, _}, _rest} -> result
           {inner_exp_node, remaining} ->
             {%AST{node_name: :unary_operator, value: next_token, left_node: inner_exp_node}, remaining}
         end
-      _ -> {{:parsing_error, :constant_missed, "Error: constant value missed"}, rest}
+
+        _ -> {{:parsing_error, :constant_missed, "Error: constant value missed"}, rest}
+    end
+
+    factor
+  end
+
+  def parse_production(tokens, operator_tokens, parse_operand) do
+    operand = parse_operand.(tokens)
+
+    final_operand = case operand do
+      {{:parsing_error, _, _}, _rest} -> operand
+
+      {_operand_node, _remaining_tokens} -> parse_loop(operand, operator_tokens, parse_operand)
+    end
+
+    final_operand
+  end
+
+  def parse_loop(left_operand, operator_tokens, parse_operand) do
+    {left_operand_node, [next_token | rest]} = left_operand
+
+    if (next_token in operator_tokens) do
+      operand = parse_operand.(rest)
+
+      case operand do
+        {{:parsing_error, _, _}, _rest} -> operand
+
+        {next_operand_node, next_operand_rest} ->
+          new_left = {%AST{node_name: :binary_operator, value: next_token, left_node: left_operand_node, right_node: next_operand_node}, next_operand_rest}
+          parse_loop(new_left, operator_tokens, parse_operand)
+      end
+
+    else
+      {left_operand_node, [next_token | rest]}
     end
   end
 end
